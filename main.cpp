1ebbf2e7bf23cf73172b965e7e4a177b9fe00235
Hadi Modarres <modarres.zadeh@gmail.com>
2018-08-22 19:44:23 +1000

    tiled fw final candidate

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-#define MAX 10000
+#define MAX 10100
 #define NOT_CONNECTED -1
 int BLOCK_SIZE=29;
 
@@ -13,7 +13,7 @@ int distance[MAX][MAX];
 int nodesCount;
 
 void Initialize() {
-    printf("nodes count: %d",nodesCount);
+//    printf("nodes count: %d",nodesCount);
     for (int i = 0; i < MAX; ++i) {
         for (int j = 0; j < MAX; ++j) {
             distance[i][j] = NOT_CONNECTED;
@@ -34,15 +34,10 @@ void FW_parallel(int i, int j, int BlockSize, int k) {
     int step = BLOCK_SIZE/omp_get_num_procs();
 
     for (int k1 = k; k1 <= k + BlockSize; ++k1) {
-      //    printf("step: %d",step);
-        //  printf ("now computing: %d %d block:%d, k:%d",i,j,BlockSize,k);
-//#pragma omp parallel for schedule(static,step)
+#pragma omp parallel for
         for (int i1 = i; i1 <= i + BlockSize - 1; ++i1) {
-            //  printf("i1: %d",i1);
             if (distance[i1][k1] != NOT_CONNECTED) {
                 for (int j1 = j; j1 <= j + BlockSize - 1; ++j1) {
-                    //        printf("j1: %d",j1);
-
                     if (distance[k1][j1] != NOT_CONNECTED &&
                         (distance[i1][j1] == NOT_CONNECTED || distance[i1][k1] + distance[k1][j1] < distance[i1][j1])) {
                         distance[i1][j1] = distance[i1][k1] + distance[k1][j1];
@@ -52,25 +47,17 @@ void FW_parallel(int i, int j, int BlockSize, int k) {
         }
 
     }
-    //   printf ("computing finished: %d %d block:%d, k:%d\n",i,j,BlockSize,k);
-    //   printf("fw done\n");
 
 }
 
 void FW(int i, int j, int BlockSize, int k) {
     if ((i>nodesCount)||(j>nodesCount))
         return;
-    // printf("FW for i,j: %d,%d blocksize: %d k:%d\n",i,j,BlockSize,k);
 
     for (int k1 = k; k1 <= k + BlockSize; ++k1) {
-        //  printf("k1: %d",k1);
-        //  printf ("now computing: %d %d block:%d, k:%d",i,j,BlockSize,k);
         for (int i1 = i; i1 <= i + BlockSize - 1; ++i1) {
-            //  printf("i1: %d",i1);
             if (distance[i1][k1] != NOT_CONNECTED) {
                 for (int j1 = j; j1 <= j + BlockSize - 1; ++j1) {
-                    //        printf("j1: %d",j1);
-
                     if (distance[k1][j1] != NOT_CONNECTED &&
                         (distance[i1][j1] == NOT_CONNECTED || distance[i1][k1] + distance[k1][j1] < distance[i1][j1])) {
                         distance[i1][j1] = distance[i1][k1] + distance[k1][j1];
@@ -80,14 +67,14 @@ void FW(int i, int j, int BlockSize, int k) {
         }
 
     }
-    //   printf ("computing finished: %d %d block:%d, k:%d\n",i,j,BlockSize,k);
-    //   printf("fw done\n");
 
 }
 
 void parallel_diameter(){
     int stat_step = nodesCount/(BLOCK_SIZE*omp_get_num_procs());
-    printf("stat step: %d\n",stat_step);
+    if (stat_step==0){
+        stat_step=1;
+    }
 
     double timeBegin, timeEnd;
     timeBegin = omp_get_wtime();
@@ -96,63 +83,40 @@ void parallel_diameter(){
         if (k*BLOCK_SIZE>nodesCount){
             break;
         }
-        //     printf("big loop\n");
-//#pragma omp parallel
-//        {
-
-//#pragma omp single
 
 
         FW(BLOCK_SIZE * k, BLOCK_SIZE * k, BLOCK_SIZE, k * BLOCK_SIZE);
 #pragma omp parallel for schedule(dynamic,stat_step)
         for (int i = 0; i < BLOCK_SIZE * k; i = i + BLOCK_SIZE) {
-            //   printf("here1");
-
-//            if ((i>nodesCount)||(k*BLOCK_SIZE>nodesCount)){
-//                continue;
-//            }
-
             FW(i, k * BLOCK_SIZE, BLOCK_SIZE, k * BLOCK_SIZE);
-
 
         }
 #pragma omp parallel for schedule(dynamic,stat_step)
         for (int i = BLOCK_SIZE * (k + 1); i < MAX; i = i + BLOCK_SIZE) {
-
-
-            //        printf("here2");
             if ((i>nodesCount)||(k*BLOCK_SIZE>nodesCount)){
                 continue;
             }
             FW(i, k * BLOCK_SIZE, BLOCK_SIZE, k * BLOCK_SIZE);
 
-
         }
 #pragma omp parallel for schedule(dynamic,stat_step)
         for (int j = 0; j < BLOCK_SIZE * k; j = j + BLOCK_SIZE) {
-            //         printf("here3");
-
 
             FW(k * BLOCK_SIZE, j, BLOCK_SIZE, k * BLOCK_SIZE);
-
 
         }
 #pragma omp parallel for schedule(dynamic,stat_step)
         for (int j = BLOCK_SIZE * (k + 1); j < MAX; j = j + BLOCK_SIZE) {
-            //       printf("here4");
             if ((j>nodesCount)||(k*BLOCK_SIZE>nodesCount)){
                 continue;
             }
             FW(k * BLOCK_SIZE, j, BLOCK_SIZE, k * BLOCK_SIZE);
 
-
         }
-//#pragma omp barrier
 #pragma omp parallel for schedule(dynamic,stat_step)
         for (int i = 0; i < MAX; i = i + BLOCK_SIZE) {
             for (int j = 0; j < MAX; j = j + BLOCK_SIZE) {
                 if ((i != k * BLOCK_SIZE) && (j != k * BLOCK_SIZE)) {
-                    //             printf("here5");
                     if ((i>nodesCount)||(j>nodesCount)){
                         continue;
                     }
@@ -164,25 +128,10 @@ void parallel_diameter(){
             }
         }
 
-//    }
-
     }
-    //Floyd-Warshall
-//    for (int k=1;k<=nodesCount;++k){
-//        for (int i=1;i<=nodesCount;++i){
-//            if (distance[i][k]!=NOT_CONNECTED){
-//                for (int j=1;j<=nodesCount;++j){
-//                    if (distance[k][j]!=NOT_CONNECTED && (distance[i][j]==NOT_CONNECTED || distance[i][k]+distance[k][j]<distance[i][j])){
-//                        distance[i][j]=distance[i][k]+distance[k][j];
-//                    }
-//                }
-//            }
-//        }
-//    }
 
     int diameter = -1;
 
-    //look for the most distant pair
     for (int i = 1; i <= nodesCount; ++i) {
         for (int j = 1; j <= nodesCount; ++j) {
             if (diameter < distance[i][j]) {
@@ -231,10 +180,6 @@ int optimizeBlockSize(){
 
 int main(int argc, char **argv) {
 
-    int i = 2;
-    for (; i <= 5; ++i) {
-        printf("i");
-    }
 
     if (argc != 2) {
         printf("The path to the input file is not specified as a parameter.\n");
@@ -246,29 +191,23 @@ int main(int argc, char **argv) {
         return -1;
     }
 
-//    double start = omp_get_wtime();
     fscanf(in_file, "%d", &nodesCount);
 
     Initialize();
-    optimizeBlockSize();
-
-    return 0;
 
 
-    printf("nodes count read: %d", nodesCount);
+
+//    printf("nodes count read: %d", nodesCount);
     int a, b, c;
-  //  int count=0;
     while (fscanf(in_file, "%d %d %d", &a, &b, &c) != EOF) {
         if (a > nodesCount || b > nodesCount) {
             printf("Vertex index out of boundary.");
             return -1;
         }
         distance[a][b] = c;
-       // count++;
-       /// printf("read %d\n",count);
     }
 
-       printf("done reading");
+//       printf("done reading");
 
         parallel_diameter();
 
